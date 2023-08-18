00eabe02691b7b18f3597f2f5820d15624fe0612
Andre Furlan <ensismoebius@gmail.com>
2023-08-18 15:56:00 -0300

    Preparing to make a calculator

--- app/src/main/java/org/dedira/calculadora/MainActivity.java
+++ app/src/main/java/org/dedira/calculadora/MainActivity.java
@@ -1,13 +1,13 @@
 package org.dedira.calculadora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtNumero01, txtNumero02;
