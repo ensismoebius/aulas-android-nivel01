f462d344222cb3ff1d6c03a6f36f551c6c3dbea1
Andre Furlan <ensismoebius@gmail.com>
2023-09-01 18:53:18 -0300

    Line comments

--- app/src/main/java/org/dedira/calculadora/ResultadoAdaptador.java
+++ app/src/main/java/org/dedira/calculadora/ResultadoAdaptador.java
@@ -12,35 +12,30 @@ import java.util.ArrayList;
 
 public class ResultadoAdaptador extends ArrayAdapter<String> {
 
-    public ResultadoAdaptador(@NonNull Context contexto, ArrayList<String> lista) {
-        super(contexto, 0, lista);
+    public ResultadoAdaptador(@NonNull Context context, ArrayList<String> lista) {
+        super(context, 0, lista);
     }
 
-    public View getView(
-            int posicao,
-            View visaoPreCarregada,
-            ViewGroup pai
-    ){
+    /**
+     * @param posicao      A posição do item dentro do conjunto de dados cuja visualização desejamos.
+     * @param precarregado A View antiga para reutilizar, se possível.
+     *                     Observação: Você deve verificar se esta View
+     *                     não é nula e é do tipo apropriado antes de usá-la.
+     *                     Se não for possível usar esta View para exibir os dados,
+     *                     este método pode criar uma nova View.
+     * @param pai          O pai ao qual esta View será eventualmente anexada
+     * @return View
+     */
+    public View getView(int posicao, View precarregado, ViewGroup pai) {
 
-        String conta = getItem(posicao);
+        // O valor que deve ser exibido
+        String expressaoMatematica = this.getItem(posicao);
 
-        if(visaoPreCarregada == null){
-            visaoPreCarregada =
-                    LayoutInflater.from(getContext()).inflate(
-                            R.layout.linha,
-                            pai,
-                            false);
+        // Cria uma nova View caso a antiga seja
+        // impossível reutilizar ou não exista
+        if (precarregado == null) {
+            precarregado = LayoutInflater.from(this.getContext()).inflate(R.layout.linha, pai, false);
         }
 
     }
-
-
-
-
 }
-
-
-
-
-
-
